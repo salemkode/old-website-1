56587bf64622c7b7411c88ee04d08a79b37cac2d
superastorh <salem.m.s2030@gmail.com>
2020-11-17 21:39:17 +0300

    update and support github page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//globle
+var body = document.querySelector("body");
 //header
 var navBar = document.querySelector("#navMain");
 var navUl = document.querySelector("#navUl");
@@ -5,30 +7,34 @@ var navToggler = document.querySelector("#navToggler");
 //article
 var cardpro = document.querySelector("#cardpro");
 var article = document.querySelector("article");
+var nextCard = document.querySelector("#next_pro");
 //footer
 var footer = document.querySelector("footer");
-
+var fooCard = document.querySelectorAll(".foo_card");
+var fooToggler = document.querySelector("#fooToggler");
+var a = document.querySelectorAll("a");
 
 onResizeEvent();
 
 window.addEventListener("resize", onResizeEvent, true);
-navToggler.addEventListener("click" , navTogglerClick)
+navToggler.addEventListener("click", navTogglerClick);
+fooToggler.addEventListener("click", fooTogglerClick);
 
 function onResizeEvent() {
-  if (window.innerWidth >= 442) { //desktop
+  if (window.innerWidth >= 442) {
+    //desktop
     //header
     navBar.style.flexDirection = "row";
     navUl.style.marginRight = "0px";
     navToggler.style.display = "none";
+    navUl.style.display = "inline";
 
     //article
     article.style.flexDirection = "row";
     cardpro.style.width = "40%";
-    //footer
-    footer.style.flexDirection = "row";
-
-
-  } else { //phone
+    nextCard.style.width = "60%";
+  } else {
+    //phone
     //header
     navBar.style.flexDirection = "column";
     navUl.style.marginRight = "auto";
@@ -38,33 +44,59 @@ function onResizeEvent() {
     //article
     article.style.flexDirection = "column";
     cardpro.style.width = "100%";
+    nextCard.style.width = "100%";
 
+  }
+
+  if (window.innerWidth >= 853) {
+    //desktop
+    fooToggler.style.display = "none";
+    //footer
+    footer.style.flexDirection = "row";
+    fooCard.forEach((element) => {
+      element.style.display = "inline";
+    });
+  } else {
+    //phone
+    fooToggler.style.display = "inline";
     //footer
     footer.style.flexDirection = "column";
 
-
+    fooCard.forEach((element) => {
+      element.style.display = "none";
+    });
   }
+  body.style.marginBottom = footer.scrollHeight + "px";
+
 }
 
 function navTogglerClick() {
-  if(navUl.style.display == "none"){
-  navUl.style.display = "inline";
-  navToggler.classList.replace("fa-arrow-circle-down" , "fa-arrow-circle-up");
-  }else{
+  if (navUl.style.display == "none") { 
+    navUl.style.display = "inline";
+    navToggler.classList.replace("fa-arrow-circle-down", "fa-arrow-circle-up");
+  } else {
     navUl.style.display = "none";
-    navToggler.classList.replace("fa-arrow-circle-up" , "fa-arrow-circle-down");
+    navToggler.classList.replace("fa-arrow-circle-up", "fa-arrow-circle-down");
+  }
+}
+function fooTogglerClick() {
+  if (fooCard[0].style.display == "none") {
+    fooCard.forEach((element) => {
+      element.style.display = "inline";
+    });
+
+    fooToggler.classList.replace("fa-arrow-circle-down", "fa-arrow-circle-up");
+  } else {
+    fooCard.forEach((element) => {
+      element.style.display = "none";
+    });
+
+    fooToggler.classList.replace("fa-arrow-circle-up", "fa-arrow-circle-down");
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-5
+a.forEach((element) => {
+  if (element.href != window.location.href + "#") {
+    element.target = "blank";
+  }
+});
