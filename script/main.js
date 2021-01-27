c460e5c307e5e426d982adcc1bfd82a32b5c49f7
superastorh <salem.m.s2030@gmail.com>
2021-01-27 12:36:22 +0300

    update 1

---
+++ script/main.js
@@ -0,0 +1,17 @@
+ /*let html_bolit = document.querySelectorAll(".header-area li");
+let myBolit = document.querySelectorAll("section");
+html_bolit = Array.from(html_bolit);
+
+
+html_bolit.forEach( (li) => {
+
+    li.onclick = event => {
+        console.log( myBolit[html_bolit.indexOf(li)] );
+        console.log("click")
+        myBolit[html_bolit.indexOf(li)].scrollIntoView({
+          behavior: "smooth"
+        });
+    }
+});
+*/
+
