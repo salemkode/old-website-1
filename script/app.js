38f248070204e5ac801faaf78f7c09561546d261
superastorh <salem.m.s2030@gmail.com>
2021-01-30 20:42:31 +0300

    new my skill page and update and fix more things

--- script/app.js
+++ script/app.js
@@ -1,80 +1,94 @@
-let list = new Vue({
-    el: '.pro-page',
-    data: {
-        ul: [
-            {
+/*page product (filter , add)*/
+let myProduct = [
+    {
 
-                img:
-                    `src/bot.jpg`,
-                text:
-                    `عبود للشاي و القهوه
+        type: 1,
+        title: "بوت للقهوة",
+        img:
+            `src/bot.jpg`,
+        text:
+            `عبود للشاي و القهوه
 
                     بوت للتسلية في المجموعات`,
-                url: "https://t.me/artea_bot",
-                type: 1,
-                title:"بوت للقهوة"
-            },
-            {
 
-                img: `src/alif.jpg`,
+        url: "https://t.me/artea_bot"
+    },
+    {
 
-                text: `موقع لغة الف البرمجية
+        type: 2,
+
+        title: "موقع للغة الف",
+
+        img: `src/alif.jpg`,
+
+        text: `موقع لغة الف البرمجية
 
             محاولة عمل اعاد تصميم لموقع لغه الف البرمجية`,
 
-                url: "https://superastorh.github.io/aliflang/",
-                type: 2,
-                title:"موقع للغة الف"
+        url: "https://superastorh.github.io/aliflang/"
 
-            },
-
-        ],
-        type: 0
-        /*
-        * 0 = all
-        * 1 = bot
-        * 2 = web
-        * */
     },
-    methods: {
-        clicked(e, int) {
-            let imgLi = document.querySelectorAll(".img-list > *");
-            imgLi.forEach(e => e.style.opacity = '0')
-            setTimeout(
-                () => {
 
-                    this.type = int;
+];
 
-                    imgLi.forEach(e => {
-                        e.style.transition = "all 0s ease";
-                        e.style.opacity = '0';
-                        e.style.transition = "all 1s ease";
-                        e.style.opacity = '1'
+let tap = $(".pro-page .type-pro >*");
+let pros;
+let imgList = $(".pro-page .img-list");
+let type = 0;
 
-                    })
-
-
-                }, 1000
-            )
-            setTimeout(
-                () => {
-                }, 2000
-            )
-
-        },
-    },
-    computed: {
-        showItem() {
-            let myShow = []
-            this.ul.forEach(
-                e => {
-                    if (this.type === e.type || this.type === 0) {
-                        myShow.push(e);
-                    }
-                }
-            )
-            return myShow;
-        }
+tap.click(
+    function (e) {
+        tap.removeClass("active");
+        this.classList.add('active');
+        type = Number.parseInt(this.getAttribute("data-type"));
+        listFilter();
     }
+);
+creatList();
 
-})
+function creatList() {
+    imgList[0].innerHTML = "";
+    myProduct.forEach(
+        (e, id) => {
+            imgList[0].innerHTML += makeHolder(id, e.title, e.text, e.img, e.type, e.url);
+        }
+    );
+    pros = $(".pro-page .img-holder");
+}
+
+function makeHolder(id, title, text, img, type, url) {
+    return `
+    <div class="img-holder" id="img-holder-${id}" data-type="${type}">
+            <a href="${url}">
+                <div class="cover"></div>
+                <div class="cover cover-p">
+                    ${text}
+                </div>
+                <div class="holder">
+                    <img src="${img}" alt="" srcset="">
+                </div>
+                <p>${title}</p>
+            </a>
+     </div>
+    `;
+}
+
+function listFilter() {
+
+    Array.from(pros).forEach(
+        e => {
+            let dataType = e.getAttribute("data-type");
+            console.log(type);
+            console.log(dataType);
+            console.log(typeof type, typeof dataType);
+            if (type == dataType || type === 0) {
+                $(e).fadeIn("slow");
+            } else {
+                setTimeout(e=> $(e).fadeOut("slow") , 700);
+            }
+        }
+    )
+
+}
+
+/*end page product */
